d9cc8be03713f48d9d1ddea06195395b6c4a128f
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-24 16:04:32 +0900

    feat(file): reader処理をtry-catchでラップ

--- src/utils/file/reader.ts
+++ src/utils/file/reader.ts
@@ -1,12 +1,27 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-export function readFileByPath({ filePath }: { filePath: string }): string {
-  const absoluteFilePath = join(process.cwd(), filePath);
-  // ファイルの内容を取得
-  const fileContent = readFileSync(absoluteFilePath, {
-    encoding: "utf-8",
-    flag: "r",
-  });
-  return fileContent;
+/**
+ * ファイルの内容を取得する
+ * @param filePath - 検索対象のファイルパス (相対パス)
+ * @returns ファイルの内容
+ */
+export function readFileByPath({
+  filePath,
+}: {
+  filePath: string;
+}): string | null {
+  try {
+    const absoluteFilePath = join(process.cwd(), filePath);
+    // ファイルの内容を取得
+    const fileContent = readFileSync(absoluteFilePath, {
+      encoding: "utf-8",
+      flag: "r",
+    });
+
+    return fileContent;
+  } catch (error) {
+    console.error(`Error reading file: ${error}`);
+    return null;
+  }
 }
