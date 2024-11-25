d95feddc678c7f1e19590a9e26ea9ce9e3bc7da0
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-26 07:29:54 +0900

    feat(consts): スキーマ関連の定数を設定ファイルに定義

---
+++ src/constants/config.ts
@@ -0,0 +1,8 @@
+export const OCCUPATIONAL_STATUS = [
+  "IPUT_TOKYO_STUDENT",
+  "COCOON_TOWER_STUDENT",
+  "STUDENT",
+  "OTHER",
+] as const;
+
+export const CONTACT_TOOL = ["EMAIL", "DISCORD", "SLACK"] as const;
