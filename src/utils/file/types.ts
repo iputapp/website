66827d5095bec4bfe1db92794dad9d7133186dbf
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-25 17:02:57 +0900

    feat(file): apiの型を定義

--- src/utils/file/types.ts
+++ src/utils/file/types.ts
@@ -1,1 +1,11 @@
+import type { FileOperationError } from "./error";
+
+export type { FileOperationError };
+
 export type FileExtension = "md" | "txt";
+
+export type FileOperationResult<T> = {
+  success: boolean;
+  data?: T;
+  error?: FileOperationError;
+};
