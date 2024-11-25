fb0df397ee9015a4d46070c49581c53d44b3c65c
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-25 14:01:06 +0900

    test(file-getter): should~は文法的にtestよりitが適切

--- src/utils/file/getter.test.ts
+++ src/utils/file/getter.test.ts
@@ -1,17 +1,17 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { MARKDOWN_DIR_PATH, MARKDOWN_FILE_EXTENSION } from "@/constants";
 import { getFilenames } from "@/utils";
 
 describe("getFilenames", () => {
-  test("Should contain 'test' file", () => {
+  it("should contain 'test' file", () => {
     const files = getFilenames({
       directoryPath: MARKDOWN_DIR_PATH,
       extension: MARKDOWN_FILE_EXTENSION,
     });
     expect(files).toContain("test");
   });
-  test("Should contain 'test.md' file with extension option", () => {
+  it("should contain 'test.md' file with extension option", () => {
     const files = getFilenames({
       directoryPath: MARKDOWN_DIR_PATH,
       extension: MARKDOWN_FILE_EXTENSION,
@@ -19,7 +19,7 @@ describe("getFilenames", () => {
     });
     expect(files).toContain("test.md");
   });
-  test("Should return empty array for non-existing directory", () => {
+  it("should return empty array for non-existing directory", () => {
     const files = getFilenames({
       directoryPath: `${MARKDOWN_DIR_PATH}/non-existing`,
       extension: MARKDOWN_FILE_EXTENSION,
