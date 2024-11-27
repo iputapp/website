4e7f5d62b599a4034d69d2db05cb8fd3ef7ff4d0
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-27 15:33:44 +0900

    feat(consts): configにorigin関連を追加

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,10 @@
+export const HOST_ORIGIN_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.HOST_ORIGIN_URL || "http://localhost:3000";
+
+export const HOST_ORIGIN_DOMAIN = new URL(HOST_ORIGIN_URL).hostname;
+
 export const MARKDOWN_DIR_PATH = process.env.MARKDOWN_DIR_PATH || "markdowns";
 export const MARKDOWN_FILE_EXTENSION = "md";
 
