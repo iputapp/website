49bbeecd5ac0b28408f9fca41e1119f254755465
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-28 08:24:07 +0900

    refactor(consts): 設定関連の定数にドキュメントを追加

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,13 +1,28 @@
+/**
+ * ホストサーバのURL
+ */
 export const HOST_ORIGIN_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : process.env.HOST_ORIGIN_URL || "http://localhost:3000";
 
+/**
+ * ホストサーバのドメイン
+ */
 export const HOST_ORIGIN_DOMAIN = new URL(HOST_ORIGIN_URL).hostname;
 
+/**
+ * Markdown ファイルのディレクトリパス (相対パス)
+ */
 export const MARKDOWN_DIR_PATH = process.env.MARKDOWN_DIR_PATH || "markdowns";
+/**
+ * Markdown ファイルの拡張子
+ */
 export const MARKDOWN_FILE_EXTENSION = "md";
 
+/**
+ * 職業ステータス
+ */
 export const OCCUPATIONAL_STATUS = [
   "IPUT_TOKYO_STUDENT",
   "COCOON_TOWER_STUDENT",
@@ -15,8 +30,14 @@ export const OCCUPATIONAL_STATUS = [
   "OTHER",
 ] as const;
 
+/**
+ * 連絡ツール
+ */
 export const CONTACT_TOOL = ["EMAIL", "DISCORD", "SLACK"] as const;
 
+/**
+ * 正規リクエスト検証トークンの設定
+ */
 export const API_NOTIFY_NEWCOMER = {
   TOKEN_COOKIE_NAME: "xy-csrf-token",
   TOKEN_HEADER_NAME: "XY-CSRF-Token",
--- src/constants/languages/ja-jp.ts
+++ src/constants/languages/ja-jp.ts
@@ -1,6 +1,10 @@
 import { CONTACT_TOOL, OCCUPATIONAL_STATUS } from "@/constants";
 import { arrayToValueMap } from "@/utils";
 
+/**
+ * 職業ステータス (日本語)
+ * マップ: @/constants/config.tsの`OCCUPATIONAL_STATUS`に対応
+ */
 export const OCCUPATIONAL_STATUS_JA = arrayToValueMap(
   [...OCCUPATIONAL_STATUS],
   (value) => {
@@ -17,6 +21,10 @@ export const OCCUPATIONAL_STATUS_JA = arrayToValueMap(
   }
 );
 
+/**
+ * 連絡ツール (日本語)
+ * マップ: @/constants/config.tsの`CONTACT_TOOL`に対応
+ */
 export const CONTACT_TOOL_JA = arrayToValueMap([...CONTACT_TOOL], (value) => {
   switch (value) {
     case "EMAIL":
