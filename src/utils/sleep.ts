8f4429dad6df7927073effc81027a407ffb6853f
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-28 20:30:24 +0900

    refactor(sleep): ドキュメントを日本語化

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,6 +1,6 @@
 /**
- * Sleep for a given amount of time.
- * @param ms - The amount of time to sleep in milliseconds.
+ * 指定した時間だけ処理を停止
+ * @param ms - 停止する時間 (ミリ秒)
  */
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
