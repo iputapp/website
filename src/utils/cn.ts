cae0a0674612e3978837790e8786fc4ab18d8448
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-28 20:29:27 +0900

    refactor(cn): ドキュメントを日本語化

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,9 +2,11 @@ import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
- * Merges class names and tailwind classes.
- * @param classNames - Class names to merge.
- * @returns Merged class names.
+ * クラス名を結合
+ * * 重複するクラス名のマージ
+ * * 同タイプのTailwind CSSをマージ (上書き/右辺優先)
+ * @param classNames - 結合するクラス名
+ * @returns 結合されたクラス名
  */
 export function cn(
   ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
