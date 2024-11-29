8a0d9b266d063290ea2c33b8d9db3da401feaa43
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-29 09:58:43 +0900

    refactor(cn): ドキュメントにexampleを追加

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -7,6 +7,19 @@ import { twMerge } from "tailwind-merge";
  * * 同タイプのTailwind CSSをマージ (上書き/右辺優先)
  * @param classNames - 結合するクラス名
  * @returns 結合されたクラス名
+ * @example
+ * ```tsx
+ * <div className={cn("foo", "bar")}>Hello, world!</div>
+ * // => <div class="foo bar">Hello, world!</div>
+ *
+ * <div className={cn("text-red-500 text-blue-500")}>Hello, world!</div>
+ * // => <div class="text-blue-500">Hello, world!</div>
+ *
+ * const [isLoading, setIsLoading] = useState(false);
+ * <div className={cn("foo", isLoading ? "bar", "")}>Hello, world!</div>
+ * // isLoading=`true` => <div class="foo bar">Hello, world!</div>
+ * // isLoading=`false` => <div class="foo">Hello, world!</div>
+ * ```
  */
 export function cn(
   ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
