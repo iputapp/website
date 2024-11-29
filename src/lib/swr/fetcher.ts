66cc9b55498ec8da0a0ad89b914845d30d95657a
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-29 10:51:46 +0900

    refactor(swr): ドキュメントにexampleを追加

--- src/lib/swr/fetcher.ts
+++ src/lib/swr/fetcher.ts
@@ -3,6 +3,11 @@
  * @param url - フェッチするURL
  * @param init - フェッチオプション
  * @see {@link https://swr.vercel.app}
+ * @example
+ * ```ts
+ * import useSWR from "swr";
+ * const { data, error, isLoading } = useSWR("/api/data", fetcher);
+ * ```
  */
 export async function fetcher(url: string, init?: RequestInit) {
   return fetch(url, init).then((res) => res.json());
