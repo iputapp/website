acb0a36ea0141c408d345aea9a09143e91d842cc
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-29 10:48:04 +0900

    refactor(swr): ドキュメントの日本語化

--- src/lib/swr/fetcher.ts
+++ src/lib/swr/fetcher.ts
@@ -1,8 +1,9 @@
 /**
- * Fetcher function for SWR
- * @param key - URL to fetch data from
+ * SWRのfetcher関数
+ * @param url - フェッチするURL
+ * @param init - フェッチオプション
  * @see {@link https://swr.vercel.app}
  */
-export async function fetcher(key: string, init?: RequestInit) {
-  return fetch(key, init).then((res) => res.json());
+export async function fetcher(url: string, init?: RequestInit) {
+  return fetch(url, init).then((res) => res.json());
 }
