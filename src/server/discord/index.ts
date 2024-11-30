1e5575a2e0f0c6fc12e0d504f6b8ae26479161cc
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-30 13:08:31 +0900

    refactor(server-discord): ドキュメントを修正

--- src/server/discord/index.ts
+++ src/server/discord/index.ts
@@ -6,7 +6,7 @@ import * as webhook from "./webhook";
  * Discord 関連のユーティリティ
  * @example
  * ```ts
- * import { Discord } from "@/lib";
+ * import { Discord } from "@/server";
  * Discord.Webhook.executeWebhook();
  * ```
  */
