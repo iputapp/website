c6478a39fe864b9f095b1469c891706b314497eb
wiyco <72733890+wiyco@users.noreply.github.com>
2024-11-30 18:33:18 +0900

    Revert "refactor(consts): ホストURL未設定を簡略化"
    This reverts commit 9bf1f4bb5535bc81f4629dc15dd1624fc14da519.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,7 +4,7 @@
 export const HOST_ORIGIN_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.HOST_ORIGIN_URL || "";
+    : new URL(process.env.HOST_ORIGIN_URL || "").origin;
 
 /**
  * ホストサーバのドメイン
