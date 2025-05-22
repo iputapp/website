566ac4ab6be97313dd8a5bc3a90d5a714b249580
Shvain <takuto3987@gmail.com>
2025-05-22 16:36:25 +0900

    feat：トップの作成

---
+++ src/components/element/ButtonLink.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export function ButtonLink({
+  href,
+  text,
+}: {href:string, text:string} ) {
+  return (
+    <Link
+      href={href}
+      className="rounded-full bg-blue px-6 py-3 text-sm font-semibold text-white shadow-sm transition hover:bg-blue active:bg-blue"
+    >
+      {text}
+    </Link>
+  );
+}
